68f16e3c06753bbcb79b40372c8b0baad72e9b98
Dalen Ha <104606345+dalenha@users.noreply.github.com>
2025-05-17 06:25:49 +0000

    2nd commit

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php /* db_config.php */
-$DB_HOST = "localhost";
-$DB_USER = "cpsc332xx";   // change to the account that hosts the DB
-$DB_PASS = "YOUR_PASSWORD";
-$DB_NAME = "university";
+$DB_HOST = "mariadb";
+$DB_USER = "cs332t3";   // change to the account that hosts the DB
+$DB_PASS = "kRmw60Wv";
+$DB_NAME = "cs332t3";
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($conn->connect_error) die("Connection failed: ".$conn->connect_error);
 ?>
